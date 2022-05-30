d526b82d9dddadd7cbbf1138fa94772cf2e18f34
Scott Nason <scottmnason@gmail.com>
2022-05-29 23:47:55 -0600

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+    buildList(e.target['new-task-description'].value)
+    form.reset()
+  })
 });
+
+function buildList(listItem){
+  let li = document.createElement('li')
+  let btn = document.createElement('btn')
+  btn.addEventListener('click', deleteItem)
+  btn.textContent = 'x'
+  li.textContent = `${listItem} `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function deleteItem(e){
+  e.target.parentNode.remove()
+}
